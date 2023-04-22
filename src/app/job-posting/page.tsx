22c1a40a8f2db8c726b6932fd41baf56dea2e3fa
DamriaNeelesh <damrianeelesh@gmail.com>
2023-04-22 17:39:22 +0530

    fializing the first part of the project

---
+++ src/app/job-posting/page.tsx
@@ -0,0 +1,102 @@
+import React from "react";
+import Header from "../header/page";
+
+const jobPosting = () => {
+  return (
+    <>
+      <Header></Header>
+      <div className="dashBody">
+      <h1 style={{marginTop:'2rem',marginLeft:'5.3rem'}}>Dashboard</h1>
+        <div
+          className="upperDashboard"
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            textAlign: "center",
+            padding:'4rem'
+          }}
+        >
+          <div className="dashField">
+            <div>
+              <img src="postJob.png" />
+            </div>
+            <div className="description">Post a Job</div>
+          </div>
+          <div className="dashField">
+            <div>
+              <img src="searchApplicant.png" />
+            </div>
+            <div className="description">Screen Applicants</div>
+          </div>
+          <div className="dashField">
+            <div>
+              <img src="orinet.png" />
+            </div>
+            <div className="description">Orient</div>
+          </div>
+          <div className="dashField">
+            <div>
+              <img src="manage.png" />
+            </div>
+            <div className="description">Manage</div>
+          </div>
+          <div className="dashField">
+            <div>
+              <img src="train.png" />
+            </div>
+            <div className="description">Train</div>
+          </div>
+        </div>
+        <div
+          className="lowerDashboard"
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            textAlign: "center",
+            padding:'4rem'
+          }}
+        >
+          <div className="dashField">
+            <div>
+              <img src="payroll.png" />
+            </div>
+            <div className="description">Payroll</div>
+          </div>
+
+          <div className="dashField">
+            <div>
+              <img src="employeeSeparation.png" />
+            </div>
+            <div className="description">Employee Separation</div>
+          </div>
+
+          <div className="dashField">
+            <div>
+              <img src="employeeKit.png" />
+            </div>
+            <div className="description">Employee kit</div>
+          </div>
+
+          <div className="dashField">
+            <div>
+              <img src="getHelp.png" />
+            </div>
+            <div className="description">Get Help</div>
+          </div>
+
+          <div className="dashField">
+            <div>
+              <img src="settings.png" />
+            </div>
+            <div className="description">Settings</div>
+          </div>
+
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default jobPosting;
