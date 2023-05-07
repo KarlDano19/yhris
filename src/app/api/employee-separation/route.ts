b74d3ecdec663d838e5bca5d6299552a2e595b76
John Patrick Madrigal <jp.madrigal07@gmail.com>
2023-05-07 11:08:37 +0800

    refactor: add post endpoint

--- src/app/api/employee-separation/route.ts
+++ src/app/api/employee-separation/route.ts
@@ -4,3 +4,8 @@ import { NextResponse } from 'next/server';
 export async function GET() {
   return NextResponse.json(separationItems);
 }
+
+export async function POST(request: Request) {
+    const res = await request.json();
+    return NextResponse.json(res);
+}
