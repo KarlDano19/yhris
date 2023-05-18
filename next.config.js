3c3649f15596d11f5b6736cbd68bbfdc0a4074cc
Reuel Dave Cardines <rdcardines@gmail.com>
2023-05-18 21:02:29 +0800

    update

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    hostName: 'https://5bd3-103-62-152-147.ngrok-free.app',
+    hostName: 'https://6007-103-62-152-147.ngrok-free.app',
   },
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: '5bd3-103-62-152-147.ngrok-free.app',
+        hostname: '6007-103-62-152-147.ngrok-free.app',
         port: '',
         pathname: '**',
       },
