46052dee5950003b61936eccf94e58e71a627c41
Reuel Dave Cardines <rdcardines@gmail.com>
2023-05-24 12:59:44 +0800

    revert hostname

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    hostName: 'http://192.168.10.167:8000',
+    hostName: 'http://139.59.118.36:8000',
   },
   images: {
     remotePatterns: [
       {
         protocol: 'http',
-        hostname: '192.168.10.167',
+        hostname: '139.59.118.36',
         port: '8000',
         pathname: '**',
       },
