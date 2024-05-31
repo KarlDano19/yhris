c98ed0624ea28e783a634d5354b314ab7cfe2110
MichaelIanLabos <yahshua.labosmi@gmail.com>
2024-05-31 14:21:23 +0800

    hooks and changes on table for evalaution history

---
+++ src/app/(auth)/(employer)/manage/evaluation-history/page.tsx
@@ -0,0 +1,24 @@
+import { cookies } from 'next/headers';
+
+import { getIronSession } from 'iron-session';
+
+import Content from '@/components/pages/(auth)/employer/manage/evaluation-history/Content';
+
+import { SessionData, sessionOptions } from '@/session/lib';
+
+export const metadata = {
+  title: 'Manage - Design Benefits - Yahshua HRIS',
+};
+
+async function getSession() {
+  const session = await getIronSession<SessionData>(cookies() as any, sessionOptions);
+  return session;
+}
+
+const DesignBenefitsPage = async () => {
+  const session = await getSession();
+  const hasActiveSubscription = session.hasActiveSubscription;
+  return <Content hasActiveSubscription={hasActiveSubscription} />;
+};
+
+export default DesignBenefitsPage;
---
+++ src/components/pages/(auth)/employer/manage/evaluation-history/hooks/useGetEvaluationHistoryItems.tsx
@@ -0,0 +1,44 @@
+import { useQuery } from '@tanstack/react-query';
+import { getCookie } from 'cookies-next';
+
+async function getEvaluationHistoryItems(filters: any) {
+  try {
+    let newFilters = { ...filters };
+    if (filters.from) newFilters.from = filters.from.toLocaleDateString('en-CA');
+    if (filters.to) newFilters.to = filters.to.toLocaleDateString('en-CA');
+    const searchParams = new URLSearchParams(newFilters);
+    const token = getCookie('token');
+    const config = {
+      method: 'GET',
+      headers: {
+        'content-type': 'application/json',
+        Authorization: `Token ${token}`,
+      },
+    };
+    if (token) {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/evaluations-history/?${searchParams}`, config);
+      if (!res.ok) {
+        throw res.json();
+      }
+      return res.json();
+    }
+    return [];
+  } catch (err: any) {
+    let errStringify = await err;
+    if (Object.hasOwn(errStringify, 'response')) {
+      throw errStringify.response.data.message;
+    }
+    throw errStringify.message;
+  }
+}
+
+function useGetEvaluationHistoryItems(filters: any) {
+  const query = useQuery(['evaluationHistoryListItemsCache'], () => getEvaluationHistoryItems(filters), {
+    refetchOnWindowFocus: false,
+    keepPreviousData: true,
+  });
+
+  return query;
+}
+
+export default useGetEvaluationHistoryItems;
