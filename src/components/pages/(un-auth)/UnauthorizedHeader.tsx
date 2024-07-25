35980f35fd705e3021beb6522328327fadbfe14b
Michael Labos <yahshua.labosmi@gmail.com>
2024-07-25 15:55:09 +0800

    tooltip import arrangement

--- src/components/pages/(un-auth)/UnauthorizedHeader.tsx
+++ src/components/pages/(un-auth)/UnauthorizedHeader.tsx
@@ -1,11 +1,10 @@
 'use client';
-
 import { Popover } from '@headlessui/react';
-
-import { Tooltip } from 'react-tooltip';
 
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
+
+import { Tooltip } from 'react-tooltip';
 
 import classNames from '@/helpers/classNames';
 
