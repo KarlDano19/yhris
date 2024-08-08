08195606c8d0505121853e138d48141da53d2301
Michael Labos <yahshua.labosmi@gmail.com>
2024-08-08 17:01:54 +0800

    dashboard responsiveness

--- src/components/button/FloatingButton.tsx
+++ src/components/button/FloatingButton.tsx
@@ -6,7 +6,7 @@ import ChatBubbleIcon from "@/svg/ChatBubbleIcon";
 const Fab = () => {
     return (
     <div>
-        <div className="fixed right-5 bottom-5 text-white bg-blue-600 rounded-xl hover:cursor-pointer" onClick={() => {window.open("https://docs.google.com/forms/d/e/1FAIpQLSfshLBcafYQvTgAjcmYu_0AyC8IuuoyXrA5LJtME_kD1oDLww/viewform", "_blank")}}>
+        <div className="fixed right-5 bottom-5 text-white bg-blue-600 rounded-xl hover:cursor-pointer z-10" onClick={() => {window.open("https://docs.google.com/forms/d/e/1FAIpQLSfshLBcafYQvTgAjcmYu_0AyC8IuuoyXrA5LJtME_kD1oDLww/viewform", "_blank")}}>
             <div className="flex flex-row items-center py-3 px-5 gap-x-2">
                 <button id="settings-fab">
                     <ChatBubbleIcon />
