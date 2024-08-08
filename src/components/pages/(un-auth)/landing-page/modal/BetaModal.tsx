6f684c389a955a5f855c2b1e89b6a8678804ddd8
Michael Labos <yahshua.labosmi@gmail.com>
2024-08-08 09:54:45 +0800

    spacing

--- src/components/pages/(un-auth)/landing-page/modal/BetaModal.tsx
+++ src/components/pages/(un-auth)/landing-page/modal/BetaModal.tsx
@@ -41,7 +41,7 @@ export default function BetaModal({
               leaveTo='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
             >
               <Dialog.Panel className='relative transform overflow-hidden rounded-lg bg-white pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg'>
-                <div className='flex pr-4 pt-4 justify-end '>
+                <div className='flex pr-4 pt-4 justify-end'>
                     <XCircleIcon className='w-6 h-6 text-[#ACB9CB] cursor-pointer mt-1' onClick={() => {setIsOpen(false)}} />
                 </div>
                 <div className='flex justify-center pb-2 md:pt-8 md:pb-2'>
