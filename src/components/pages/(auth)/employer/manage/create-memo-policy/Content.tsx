2964786a508c5fc9837964b51d1f3f590d6cd66b
Khenpaler <paler.khen.ustp@gmail.com>
2025-06-24 13:55:16 +0800

    create memo/policy tool tip

--- src/components/pages/(auth)/employer/manage/create-memo-policy/Content.tsx
+++ src/components/pages/(auth)/employer/manage/create-memo-policy/Content.tsx
@@ -7,6 +7,7 @@ import Link from 'next/link';
 import { useQueryClient } from '@tanstack/react-query';
 import { Menu, Transition } from '@headlessui/react';
 import toast from 'react-hot-toast';
+import { Tooltip } from 'react-tooltip';
 
 import Pagination from '@/components/Pagination';
 import CustomDatePicker from '@/components/CustomDatePicker';
@@ -322,9 +323,12 @@ const Content = ({ hasActiveSubscription }: { hasActiveSubscription: boolean }) 
               <div className='flex-none w-11/12 lg:w-1/3'>
                 <div className='relative flex items-center'>
                   <input
-                    type='text'
+                  type='text'
                   name='search'
                   id='search'
+                  data-tooltip-id='search-tooltip'
+                  data-tooltip-content='Search for Memo: Title'
+                  data-tooltip-place='bottom'
                   className='block w-full rounded-md border-0 py-1.5 px-3 pr-14 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6'
                   onChange={(e) => setItemsFilter({ ...itemsFilter, search: e.target.value })}
                   placeholder='Search ...'
@@ -449,6 +453,8 @@ const Content = ({ hasActiveSubscription }: { hasActiveSubscription: boolean }) 
         // isLoading={false}
         isLoading={isLoading}
       />
+
+      <Tooltip id='search-tooltip'/>
     </>
   );
 };
