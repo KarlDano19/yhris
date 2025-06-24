b9a91317021b30da1773c17b564d8c4dec2dcd7e
Khenpaler <paler.khen.ustp@gmail.com>
2025-06-24 17:06:04 +0800

    annual added date range search removed search field

--- src/components/pages/(auth)/employer/dole/annual-medical-report/hooks/useGetAnnualMedicalReportItems.tsx
+++ src/components/pages/(auth)/employer/dole/annual-medical-report/hooks/useGetAnnualMedicalReportItems.tsx
@@ -17,6 +17,8 @@ async function getAnnualMedicalReportItems(filters: any) {
     if (filters.search) newFilters.search = filters.search;
     if (filters.from) newFilters.from = filters.from.toLocaleDateString("en-CA");
     if (filters.to) newFilters.to = filters.to.toLocaleDateString("en-CA");
+    if (!newFilters.from) delete newFilters.from;
+    if (!newFilters.to) delete newFilters.to;
     const searchParams = new URLSearchParams(
       Object.entries(newFilters).map(([key, value]) => [key, String(value)])
     );
