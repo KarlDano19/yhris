a18ec6a9043fc2461af4f25a3e98d0afd8d4cff6
Khenpaler <paler.khen.ustp@gmail.com>
2025-07-18 17:06:07 +0800

    hired applicants pagination

--- src/components/pages/(auth)/employer/hired-applicants/Content.tsx
+++ src/components/pages/(auth)/employer/hired-applicants/Content.tsx
@@ -1,9 +1,16 @@
 'use client';
-import { ArrowLeftIcon } from '@heroicons/react/24/solid';
-import Link from 'next/link';
+
 import React, { useEffect, useState } from 'react';
-import useGetHiredApplicants from './hooks/useGetHiredApplicants';
+import { useRef } from 'react';
+
+import Link from 'next/link';
+
 import Pagination from '@/components/Pagination';
+import useGetHiredApplicants from './hooks/useGetHiredApplicants';
+
+import { ArrowLeftIcon } from '@heroicons/react/24/solid';
+import { MagnifyingGlassIcon } from '@heroicons/react/24/solid';
+
 
 type PaginationProps = {
   totalRecords: number;
@@ -25,9 +32,19 @@ const Content = () => {
     pageSize: pageSize,
     currentPage: currentPage,
   });
+  const lastSearchedValue = useRef(itemsFilter.search || '');
   useEffect(() => {
     refetch();
   }, [currentPage, pageSize]);
+
+  useEffect(() => {
+    if (data && data.total_pages && data.total_records) {
+      setPagination({
+        totalPages: data.total_pages,
+        totalRecords: data.total_records,
+      });
+    }
+  }, [data]);
 
   const paginationChange = (event: any) => {
     const newCurrentPage = event.selected + 1;
@@ -48,6 +65,47 @@ const Content = () => {
       </div>
       <div className='px-2 md:px-8 lg:px-4'>
         <h2 className='text-xl font-bold text-indigo-dye'>Hired Applicants</h2>
+        {/* Search bar */}
+        <div className='mt-6 mb-10 flex flex-col lg:flex-row items-left gap-4'>
+          <div className='flex gap-2 lg:w-1/3 pr-5 md:pr-16'>
+            <div className='flex-none w-11/12 lg:w-full'>
+              <div className='relative flex items-center'>
+                <input
+                  type='text'
+                  name='search'
+                  id='search'
+                  className='block w-full rounded-md border-0 py-1.5 px-3 pr-14 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6'
+                  value={itemsFilter.search}
+                  onChange={(e) => {
+                    const newValue = e.target.value;
+                    setItemsFilter({ ...itemsFilter, search: newValue });
+                    // If the previous value was not empty and the new value is empty, refetch
+                    if (lastSearchedValue.current !== '' && newValue === '') {
+                      refetch();
+                      lastSearchedValue.current = '';
+                    }
+                  }}
+                  onKeyDown={(e) => {
+                    if (e.key === 'Enter') {
+                      const value = e.currentTarget.value;
+                      if (value !== lastSearchedValue.current) {
+                        refetch();
+                        lastSearchedValue.current = value;
+                      }
+                    }
+                  }}
+                  placeholder='Search ...'
+                />
+              </div>
+            </div>
+            <button
+              className='bg-white border border-gray-300 rounded-md p-2 ml-1 hover:bg-gray-100'
+              onClick={() => refetch()}
+            >
+              <MagnifyingGlassIcon className='h-5 w-5' />
+            </button>
+          </div>
+        </div>
         <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6'>
           {!isLoading && data?.records
             ? data.records.map((hiredApplicant: any, index: number) => (
