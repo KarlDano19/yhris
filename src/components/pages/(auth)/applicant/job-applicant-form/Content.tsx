4dfb7d84b1c11c114470f1c1f185d80e3421b9c9
Earl Dave <elaco.earl13@gmail.com>
2025-08-02 11:14:05 +0800

    Fixed placement of the Job Title - Application Form

--- src/components/pages/(auth)/applicant/job-applicant-form/Content.tsx
+++ src/components/pages/(auth)/applicant/job-applicant-form/Content.tsx
@@ -99,7 +99,7 @@ const Content = () => {
 
   return (
     <div className={`mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 `}>
-      <div className='px-4 pt-8'>
+      <div className='px-4 pt-24'>
         <h4 className='text-lg md:text-2xl font-bold md:font-semibold'>
           Jobs - {jobDetailData?.job_title} | Application Form
         </h4>
