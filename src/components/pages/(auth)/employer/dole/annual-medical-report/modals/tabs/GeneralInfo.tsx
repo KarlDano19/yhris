163bfc6504ad29d4c8c6ef43799fa48303782a26
Khenpaler <paler.khen.ustp@gmail.com>
2025-08-14 19:37:40 +0800

    change name

--- src/components/pages/(auth)/employer/dole/annual-medical-report/modals/tabs/GeneralInfo.tsx
+++ src/components/pages/(auth)/employer/dole/annual-medical-report/modals/tabs/GeneralInfo.tsx
@@ -101,7 +101,7 @@ function GeneralInfo({
               <input
                 type="text"
                 readOnly
-                {...register("company_name", { required: true })}
+                {...register("name_of_establishment", { required: true })}
                 id="company_name"
                 className="cursor-not-allowed rounded-md w-full border-0 px-3 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:black sm:text-sm sm:leading-6"
               />
