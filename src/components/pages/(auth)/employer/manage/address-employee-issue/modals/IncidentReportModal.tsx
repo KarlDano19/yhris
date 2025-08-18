7be85ecd68618420af889e8ce062d187688c8521
Khenpaler <paler.khen.ustp@gmail.com>
2025-08-18 16:39:01 +0800

    address employee issue no click outside close

--- src/components/pages/(auth)/employer/manage/address-employee-issue/modals/IncidentReportModal.tsx
+++ src/components/pages/(auth)/employer/manage/address-employee-issue/modals/IncidentReportModal.tsx
@@ -97,7 +97,7 @@ export default function IncidentReportModal({
 
   return (
     <Transition.Root show={isOpen} as={Fragment}>
-      <Dialog as='div' className='relative z-10' initialFocus={cancelButtonRef} onClose={setIsOpen}>
+      <Dialog as='div' className='relative z-10' initialFocus={cancelButtonRef} onClose={() => {}}>
         <Transition.Child
           as={Fragment}
           enter='ease-out duration-300'
