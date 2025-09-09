f10695bfdb44b4fe6bcb7c4ab19cb29720889902
Khenpaler <paler.khen.ustp@gmail.com>
2025-09-09 13:53:47 +0800

    Enhance NTEAttachmentViewModal header styling
    - Added rounded top corners to the header for a more polished appearance.

--- src/components/pages/(auth)/employer/manage/address-employee-issue/modals/NTEAttachmentViewModal.tsx
+++ src/components/pages/(auth)/employer/manage/address-employee-issue/modals/NTEAttachmentViewModal.tsx
@@ -48,7 +48,7 @@ function NTEAttachmentViewModal({ isOpen, setIsOpen }: { isOpen: any; setIsOpen:
                 leaveTo='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
               >
                 <Dialog.Panel className='relative transform overflow-visible rounded-lg bg-white pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-4xl'>
-                  <div className='flex bg-savoy-blue p-2 items-center'>
+                  <div className='flex bg-savoy-blue p-2 items-center rounded-t-lg'>
                     <h3 className='flex-1 text-white ml-2 font-semibold'>NTE Attachment</h3>
                     <XCircleIcon className='w-8 h-8 text-white cursor-pointer' onClick={customCloseModal} />
                   </div>
