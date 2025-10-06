cf0fd0eb63aaeb7c102f63950c06cf50b8fd283a
Khen <paler.khen.ustp@gmail.com>
2025-10-06 17:42:35 +0800

    fix: update recipient photo layout in evaluation scheduler
    - Adjusted the layout of recipient photos in the evaluation scheduler to improve visual alignment and responsiveness.
    - Changed the class for the recipient photo container to set a maximum width and ensure proper item alignment.

--- src/components/pages/(auth)/employer/train/evaluation/evaluation-scheduler/Content.tsx
+++ src/components/pages/(auth)/employer/train/evaluation/evaluation-scheduler/Content.tsx
@@ -189,7 +189,7 @@ function Content({ hasActiveSubscription }: { hasActiveSubscription: boolean }) 
   const renderRecipientsRoundPhoto = (recipients: any) => {
     return (
       <div 
-        className='inline-flex w-40 overflow-x-auto overflow-y-visible pb-2.5'
+        className='inline-flex items-center overflow-x-auto overflow-y-visible pb-2.5 max-w-[200px]'
         style={{
           scrollbarWidth: 'thin',
           scrollbarColor: '#2d3e58 #f1f1f1'
@@ -201,8 +201,7 @@ function Content({ hasActiveSubscription }: { hasActiveSubscription: boolean }) 
               key={index}
               className='w-[40px] h-[40px] border-2 border-[#fff] rounded-full relative flex-shrink-0'
               style={{ 
-                left: `${4 * index}px`,
-                zIndex: recipients.length - index
+                marginRight: '4px'
               }}
             >
               <RecipientAvatar recipient={recipient} size={40} />
