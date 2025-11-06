a5bd4e244ea9f6b29aba3fa632646aec802eb6ce
anonymousearly <elaco.earl13@gmail.com>
2025-11-06 14:58:57 +0800

    fix: adjust padding in datepicker components for better alignment
    - Updated padding in the datepicker input fields to improve visual consistency and user experience.

--- src/components/pages/(auth)/employer/screen-applicants/[id]/Content.tsx
+++ src/components/pages/(auth)/employer/screen-applicants/[id]/Content.tsx
@@ -594,7 +594,7 @@ export default function Content({ hasActiveSubscription }: { hasActiveSubscripti
                           id='from-datepicker'
                           placeholder={'mm/dd/yyyy'}
                           className={
-                            'appearance-none block w-full rounded-md py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-black sm:text-sm sm:leading-6'
+                            'appearance-none block w-full rounded-md py-1.5 pl-3 pr-10 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-black sm:text-sm sm:leading-6'
                           }
                           selected={dateFrom}
                           pickerOnChange={handleDateFromChange}
@@ -607,7 +607,7 @@ export default function Content({ hasActiveSubscription }: { hasActiveSubscripti
                           id='to-datepicker'
                           placeholder={'mm/dd/yyyy'}
                           className={
-                            'appearance-none block w-full rounded-md py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-black sm:text-sm sm:leading-6'
+                            'appearance-none block w-full rounded-md py-1.5 pl-3 pr-10 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-black sm:text-sm sm:leading-6'
                           }
                           selected={dateTo}
                           pickerOnChange={handleDateToChange}
