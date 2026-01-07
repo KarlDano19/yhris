7839492cb1a75a0b3b8078c4a9dd251255464d32
anonymousearly <elaco.earl13@gmail.com>
2026-01-07 11:24:20 +0800

    Fixed Content alignment with the Header

--- src/components/pages/(auth)/yahshua-connect/tabs/business-mode/BusinessModeLayout.tsx
+++ src/components/pages/(auth)/yahshua-connect/tabs/business-mode/BusinessModeLayout.tsx
@@ -124,7 +124,7 @@ const BusinessModeLayout = ({ children }: BusinessModeLayoutProps) => {
   return (
     <>
       <FloatingMenuBar />
-      <div className="max-w-[90rem] mx-auto px-4 sm:px-6 lg:px-8 py-8 pb-32">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 pb-32">
         <div className="grid grid-cols-1 lg:grid-cols-12 gap-6">
           {/* Left Sidebar */}
           <div className="lg:col-span-3">
